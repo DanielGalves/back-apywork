6ac87e7fcffd66f3b39f0a557702e9eec92f132f
Daniel Gama Alves <danielgama1@live.com>
2026-01-08 00:28:57 -0300

    chore: envio do projeto base springboot

---
+++ src/main/java/br/com/apywork/apywork_backend/ApyworkBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.apywork.apywork_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApyworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApyworkBackendApplication.class, args);
+	}
+
+}
